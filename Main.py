0dde333d03b07d5ac7435451f35b4f6febb2aa48
siddhanth78 <siddhanth.lalgowdar@gmail.com>
2022-04-14 17:13:44 -0600

    Initial commit

---
+++ Main.py
@@ -0,0 +1,44 @@
+import os
+import os.path
+import time
+import display_module as disp
+
+#-----------------------------------------------------------------------------------
+
+path = os.getcwd() #current working dir
+
+options = ['Story', 'Duel', 'Market', 'Trade', 'Exit'] #menu options
+
+menu = disp.menu(title = "Main Menu",
+                items = options,
+                reuse = 1
+                    )       #main menu
+
+#-----------------------------------------------------------------------------------
+
+os.system('cls')
+
+while True:
+    print(menu)
+    choice = input("Enter your choice: ")
+    
+    if(choice == "1"):
+        print("COMING SOON\n")
+    elif(choice == "2"):
+        print("COMING SOON\n")
+    elif(choice == "3"):
+        os.system(path+"\\Market.py")
+    elif(choice == "4"):
+        print("COMING SOON\n")
+    elif(choice == "5"):
+        confirm = disp.yesno()
+        
+        if(confirm == 1):
+            break
+        elif(confirm == 0):
+            continue
+        elif(confirm == 2):
+            disp.error("Invalid choice.")
+            continue
+    else:
+        disp.error("Invalid choice.")
