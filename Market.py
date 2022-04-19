ab6359418ed5744be657869048895e94e0344f5f
siddhanth78 <siddhanth.lalgowdar@gmail.com>
2022-04-19 11:14:23 -0600

    improved character duplicate system, fixed some bugs

--- Market.py
+++ Market.py
@@ -92,6 +92,9 @@ while True:
     choice = input("Enter your choice: ")
     
     if(choice == "1"):
+        b = bitdotio.bitdotio(api)
+        conn = b.get_connection()
+        cur = conn.cursor()
         disp.warn("Search is case-sensitive.")
         
         while True:
@@ -122,7 +125,16 @@ while True:
                         m = f"Rank: {x[1]}\nAttack: {x[2]}\nHealth: {x[3]}")
                     
     elif(choice == "2"):
+        b = bitdotio.bitdotio(api)
+        conn = b.get_connection()
+        cur = conn.cursor()
+        
         while True:
+            player_chars = []
+            cur.execute(f"select ch_id, attack, health from \"siddhanth78/MainGame\".player_characters where p_id = '{player_id}'")
+            for x in cur.fetchall():
+                player_chars.append(x)
+                
             print(chambers)
             print(f"Points available: {points}\n")
             select_chamber = input("Enter your choice: ")   
@@ -185,33 +197,37 @@ while True:
                 break
                 
             dupe_flag = False
+            char_index = 0
             
-            if(random_char[0] in player_chars):
-                random_char_list = list(random_char)
-                
-                if(random_char_list[2] == 'bronze'):
-                    random_char_list[3] = random_char_list[3] + 20
-                    random_char_list[4] = random_char_list[4] + 200
-                elif(random_char_list[2] == 'silver'):
-                    random_char_list[3] = random_char_list[3] + 70
-                    random_char_list[4] = random_char_list[4] + 450
-                elif(random_char_list[2] == 'gold'):
-                    random_char_list[3] = random_char_list[3] + 130
-                    random_char_list[4] = random_char_list[4] + 900
-                elif(random_char_list[2] == 'platinum'):
-                    random_char_list[3] = random_char_list[3] + 200
-                    random_char_list[4] = random_char_list[4] + 1500
+            for x in player_chars:
+                if(random_char[0] == x[0]):
+                    char_index = player_chars.index(x)
+                    dupe_flag = True
+                    break
                     
-                random_char = tuple(random_char_list)
+            if(dupe_flag == True):
+                player_chars_list = list(player_chars[char_index])
                 
-                dupe_flag = True
+                if(random_char[2] == "bronze"):
+                    player_chars_list[1] = player_chars_list[1] + 20
+                    player_chars_list[2] = player_chars_list[2] + 200
+                elif(random_char[2] == "silver"):
+                    player_chars_list[1] = player_chars_list[1] + 70
+                    player_chars_list[2] = player_chars_list[2] + 450
+                elif(random_char[2] == "gold"):
+                    player_chars_list[1] = player_chars_list[1] + 130
+                    player_chars_list[2] = player_chars_list[2] + 900
+                elif(random_char[2] == "platinum"):
+                    player_chars_list[1] = player_chars_list[1] + 200
+                    player_chars_list[2] = player_chars_list[2] + 1500
             
             try:
                 if(dupe_flag == False):
                     cur.execute(f"insert into \"siddhanth78/MainGame\".player_characters values('{player_id}', '{random_char[0]}', {random_char[3]}, {random_char[4]}, 1)")
                     conn.commit()
                 elif(dupe_flag == True):
-                    cur.execute(f"update \"siddhanth78/MainGame\".player_characters set attack = {random_char[3]}, health = {random_char[4]} where p_id = '{player_id}' and ch_id = '{random_char[0]}'")
+                    cur.execute(f"update \"siddhanth78/MainGame\".player_characters set attack = {player_chars_list[1]} where p_id = '{player_id}' and ch_id = '{random_char[0]}'")
+                    cur.execute(f"update \"siddhanth78/MainGame\".player_characters set health = {player_chars_list[2]} where p_id = '{player_id}' and ch_id = '{random_char[0]}'")
                     conn.commit()
             except:
                 disp.error("Synthesizer not working as intended. Try again later.")
@@ -229,11 +245,7 @@ while True:
                     points = points - 155000
                 else:
                     pass
-                
-                pprint(player_chars)
-                print()
-                print(random_char)
-                
+                    
                 if(dupe_flag == True):
                     disp.box("SYNTHESIS COMPLETE", f"DUPLICATE:\n{random_char[1]}\nRank: {random_char[2]}")
                 elif(dupe_flag == False):
