bd2bc6a7063b9715856e7c757731e24d043f2c6c
siddhanth78 <siddhanth.lalgowdar@gmail.com>
2022-04-28 14:54:28 -0600

    added quit option

--- Start.py
+++ Start.py
@@ -13,11 +13,13 @@ os.system('cls')
 print("\n-----DUEL-----")
 
 while True:
-    choice = input("\n1. Login\n2. Sign up\n\nEnter your choice: ")
+    choice = input("\n1. Login\n2. Sign up\n3. Quit\n\nEnter your choice: ")
     
     if(choice == "1"):
         os.system(path+"\\Login.py")
     elif(choice == "2"):
         os.system(path+"\\SignUp.py")
+    elif(choice == "3"):
+        break
     else:
         disp.error("Invalid choice.")
